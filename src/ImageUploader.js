80a6fa9e7826fbc7ebd19fa795a150fd5932491a
kansuke <aichitead23302@gmail.com>
2023-12-15 21:15:34 +0900

    分岐分

--- src/ImageUploader.js
+++ src/ImageUploader.js
@@ -1,19 +1,139 @@
-// //ImageUploader.js
+// // //ImageUploader.js
 
+// // import React, { useState } from "react";
+// // import { Button } from "@mui/material";
+// // import ImageLogo from "./image.svg";
+// // import "./ImageUpload.css";
+// // import {
+// //   getStorage,
+// //   ref,
+// //   uploadBytes,
+// //   getDownloadURL,
+// // } from "firebase/storage";
+// // import { initializeApp } from "firebase/app";
+// // import { getFirestore, collection, addDoc } from "firebase/firestore";
+// // import { useNavigate } from "react-router-dom"; // useHistory を useNavigate に変更
+
+// // const firebaseConfig = {
+// //   apiKey: "AIzaSyCG99nl_hH_8GvqEpxGmT-Zc6xyVKHJKiI",
+// //   authDomain: "test-app-9eac0.firebaseapp.com",
+// //   databaseURL: "https://test-app-9eac0-default-rtdb.asia-southeast1.firebasedatabase.app",
+// //   projectId: "test-app-9eac0",
+// //   storageBucket: "test-app-9eac0.appspot.com",
+// //   messagingSenderId: "475645900729",
+// //   appId: "1:475645900729:web:b6b025376cf9f1f9c1a868"
+// // };
+
+// // const firebaseApp = initializeApp(firebaseConfig);
+// // const storage = getStorage(firebaseApp);
+// // const firestore = getFirestore(firebaseApp);
+
+// // const ImageUploader = () => {
+// //   const navigate = useNavigate(); // useHistory を useNavigate に変更
+
+// //   const [loading, setLoading] = useState(false);
+// //   const [isUploaded, setUploaded] = useState(false);
+// //   const [downloadURL, setDownloadURL] = useState(null);
+
+// //   const OnFileUploadToFirebase = async (e) => {
+// //     const file = e.target.files[0];
+// //     const storageRef = ref(storage, "image/" + file.name);
+
+// //     try {
+// //       await uploadBytes(storageRef, file);
+
+// //       const url = await getDownloadURL(storageRef);
+// //       setDownloadURL(url);
+
+// //       const docRef = await addDoc(collection(firestore, "messages"), {
+// //         name: "User Name",
+// //         message: url,
+// //         photoURL: "/images/profile_placeholder.png",
+// //         timestamp: new Date(),
+// //       });
+
+// //       setLoading(false);
+// //       setUploaded(true);
+// //     } catch (error) {
+// //       console.error("Error uploading file:", error);
+// //       setLoading(false);
+// //       setUploaded(false);
+// //     }
+// //   };
+
+  
+
+// //   return (
+// //     <>
+// //       {loading ? (
+// //         <h2>アップロード中・・・</h2>
+// //       ) : (
+// //         <>
+// //           {isUploaded ? (
+// //             <>
+// //               <h2>アップロード完了</h2>
+// //             </>
+// //           ) : (
+// //             <div className="outerBox">
+// //               <div className="title">
+// //                 <h2>画像アップローダー</h2>
+// //                 <p>JpegかPngの画像ファイル</p>
+// //               </div>
+// //               <div className="imageUplodeBox">
+// //                 <div className="imageLogoAndText">
+// //                   <img src={ImageLogo} alt="imagelogo" />
+// //                   <p>ここにドラッグ＆ドロップしてね</p>
+// //                 </div>
+// //                 <input
+// //                   className="imageUploadInput"
+// //                   multiple
+// //                   name="imageURL"
+// //                   type="file"
+// //                   accept=".png, .jpeg, .jpg"
+// //                   onChange={OnFileUploadToFirebase}
+// //                 />
+// //               </div>
+// //               <p>または</p>
+// //               <div className="buttonContainer">
+// //                 <Button variant="contained">
+// //                   ファイルを選択
+// //                   <input
+// //                     className="imageUploadInput"
+// //                     type="file"
+// //                     accept=".png, .jpeg, .jpg"
+// //                     onChange={OnFileUploadToFirebase}
+// //                   />
+// //                 </Button>
+// //                 {/* <Button variant="outlined" onClick={handleReturnToChat}>
+// //                   チャットに戻る
+// //                 </Button> */}
+// //               </div>
+// //             </div>
+// //           )}
+// //         </>
+// //       )}
+// //     </>
+// //   );
+// // };
+
+// // export default ImageUploader;
+
+// // 必要な依存関係とスタイルをインポート
 // import React, { useState } from "react";
 // import { Button } from "@mui/material";
-// import ImageLogo from "./image.svg";
-// import "./ImageUpload.css";
+// import ImageLogo from "./image.svg"; // ロゴのための画像ファイルをインポート
+// import "./ImageUpload.css"; // コンポーネントのスタイルをインポート
 // import {
 //   getStorage,
 //   ref,
 //   uploadBytes,
 //   getDownloadURL,
-// } from "firebase/storage";
+// } from "firebase/storage"; // Firebase Storage関数をインポート
 // import { initializeApp } from "firebase/app";
-// import { getFirestore, collection, addDoc } from "firebase/firestore";
-// import { useNavigate } from "react-router-dom"; // useHistory を useNavigate に変更
+// import { getFirestore, collection, addDoc } from "firebase/firestore"; // Firebase Firestore関数をインポート
+// import { useNavigate } from "react-router-dom"; // React Routerのナビゲーションフックをインポート
 
+// // Firebaseの設定オブジェクト。APIキーとプロジェクトの詳細が含まれています。
 // const firebaseConfig = {
 //   apiKey: "AIzaSyCG99nl_hH_8GvqEpxGmT-Zc6xyVKHJKiI",
 //   authDomain: "test-app-9eac0.firebaseapp.com",
@@ -24,45 +144,54 @@
 //   appId: "1:475645900729:web:b6b025376cf9f1f9c1a868"
 // };
 
+// // Firebaseアプリを初期化し、Firebase StorageとFirestoreのインスタンスを作成します。
 // const firebaseApp = initializeApp(firebaseConfig);
 // const storage = getStorage(firebaseApp);
 // const firestore = getFirestore(firebaseApp);
 
+// // ImageUploaderという名前の関数コンポーネント
 // const ImageUploader = () => {
-//   const navigate = useNavigate(); // useHistory を useNavigate に変更
+//   const navigate = useNavigate(); // React Routerのナビゲーションフック
 
+//   // ローディング、アップロードステータス、ダウンロードURLを管理するState変数
 //   const [loading, setLoading] = useState(false);
 //   const [isUploaded, setUploaded] = useState(false);
 //   const [downloadURL, setDownloadURL] = useState(null);
 
+//   // Firebase StorageとFirestoreへのファイルアップロードを処理する関数
 //   const OnFileUploadToFirebase = async (e) => {
-//     const file = e.target.files[0];
-//     const storageRef = ref(storage, "image/" + file.name);
+//     const file = e.target.files[0]; // 選択されたファイルを取得
+//     const storageRef = ref(storage, "test_image/" + file.name); // ストレージの場所への参照を作成
 
 //     try {
+//       // ファイルをFirebase Storageにアップロード
 //       await uploadBytes(storageRef, file);
 
+//       // アップロードされたファイルのダウンロードURLを取得
 //       const url = await getDownloadURL(storageRef);
+//       console.log("Download URL:", url);
 //       setDownloadURL(url);
 
-//       const docRef = await addDoc(collection(firestore, "messages"), {
+//       // Firestoreコレクションに新しいドキュメントを追加（サンプルデータを含む）
+//       const docRef = await addDoc(collection(firestore, "messages1"), {
 //         name: "User Name",
 //         message: url,
 //         photoURL: "/images/profile_placeholder.png",
 //         timestamp: new Date(),
 //       });
 
+//       // ローディングをfalseに設定し、アップロードステータスをtrueに更新
 //       setLoading(false);
 //       setUploaded(true);
 //     } catch (error) {
-//       console.error("Error uploading file:", error);
+//       // アップロードプロセス中のエラーを処理
+//       console.error("ファイルのアップロードエラー:", error);
 //       setLoading(false);
 //       setUploaded(false);
 //     }
 //   };
 
-  
-
+//   // 現在のステートに基づいてJSXをレンダリング
 //   return (
 //     <>
 //       {loading ? (
@@ -84,6 +213,7 @@
 //                   <img src={ImageLogo} alt="imagelogo" />
 //                   <p>ここにドラッグ＆ドロップしてね</p>
 //                 </div>
+//                 {/* ファイル選択のためのInputフィールドとイベントハンドラ */}
 //                 <input
 //                   className="imageUploadInput"
 //                   multiple
@@ -95,6 +225,7 @@
 //               </div>
 //               <p>または</p>
 //               <div className="buttonContainer">
+//                 {/* 関連付けられたInputフィールドを使用したファイル選択のためのボタン */}
 //                 <Button variant="contained">
 //                   ファイルを選択
 //                   <input
@@ -104,6 +235,7 @@
 //                     onChange={OnFileUploadToFirebase}
 //                   />
 //                 </Button>
+//                 {/* チャットに戻るための追加のボタン（コメントアウト済み） */}
 //                 {/* <Button variant="outlined" onClick={handleReturnToChat}>
 //                   チャットに戻る
 //                 </Button> */}
@@ -116,24 +248,24 @@
 //   );
 // };
 
+// // ImageUploaderコンポーネントをデフォルトエクスポート
 // export default ImageUploader;
 
-// 必要な依存関係とスタイルをインポート
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "@mui/material";
-import ImageLogo from "./image.svg"; // ロゴのための画像ファイルをインポート
-import "./ImageUpload.css"; // コンポーネントのスタイルをインポート
+import ImageLogo from "./image.svg";
+import "./ImageUpload.css";
 import {
   getStorage,
   ref,
   uploadBytes,
   getDownloadURL,
-} from "firebase/storage"; // Firebase Storage関数をインポート
+} from "firebase/storage";
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore"; // Firebase Firestore関数をインポート
-import { useNavigate } from "react-router-dom"; // React Routerのナビゲーションフックをインポート
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { useLocation, useNavigate } from "react-router-dom";
 
-// Firebaseの設定オブジェクト。APIキーとプロジェクトの詳細が含まれています。
+// Firebaseの設定情報
 const firebaseConfig = {
   apiKey: "AIzaSyCG99nl_hH_8GvqEpxGmT-Zc6xyVKHJKiI",
   authDomain: "test-app-9eac0.firebaseapp.com",
@@ -144,112 +276,151 @@ const firebaseConfig = {
   appId: "1:475645900729:web:b6b025376cf9f1f9c1a868"
 };
 
-// Firebaseアプリを初期化し、Firebase StorageとFirestoreのインスタンスを作成します。
+// Firebaseアプリの初期化とストレージ、Firestoreのインスタンス作成
 const firebaseApp = initializeApp(firebaseConfig);
 const storage = getStorage(firebaseApp);
 const firestore = getFirestore(firebaseApp);
 
-// ImageUploaderという名前の関数コンポーネント
+// ImageUploaderコンポーネント
 const ImageUploader = () => {
-  const navigate = useNavigate(); // React Routerのナビゲーションフック
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  // ローディング、アップロードステータス、ダウンロードURLを管理するState変数
   const [loading, setLoading] = useState(false);
   const [isUploaded, setUploaded] = useState(false);
   const [downloadURL, setDownloadURL] = useState(null);
 
-  // Firebase StorageとFirestoreへのファイルアップロードを処理する関数
   const OnFileUploadToFirebase = async (e) => {
-    const file = e.target.files[0]; // 選択されたファイルを取得
-    const storageRef = ref(storage, "test_image/" + file.name); // ストレージの場所への参照を作成
+    const file = e.target.files[0];
+    const storageRef = ref(storage, `test_image/${file.name}`);
 
     try {
-      // ファイルをFirebase Storageにアップロード
       await uploadBytes(storageRef, file);
-
-      // アップロードされたファイルのダウンロードURLを取得
       const url = await getDownloadURL(storageRef);
       console.log("Download URL:", url);
       setDownloadURL(url);
 
-      // Firestoreコレクションに新しいドキュメントを追加（サンプルデータを含む）
-      const docRef = await addDoc(collection(firestore, "messages1"), {
+      let collectionName = "defaultCollection"; // デフォルトのコレクション名
+
+      // URLに応じてコレクション名を設定
+      if (location.pathname === "/room1.html") {
+        collectionName = "room1Collection";
+      } else if (location.pathname === "/room2.html") {
+        collectionName = "room2Collection";
+      } else if (location.pathname === "/room3.html") {
+        collectionName = "room3Collection";
+      } else if (location.pathname === "/room4.html") {
+        collectionName = "room4Collection";
+      }
+
+      // Firestoreの指定されたコレクションに新しいドキュメントを追加
+      const docRef = await addDoc(collection(firestore, collectionName), {
         name: "User Name",
         message: url,
         photoURL: "/images/profile_placeholder.png",
         timestamp: new Date(),
       });
 
-      // ローディングをfalseに設定し、アップロードステータスをtrueに更新
       setLoading(false);
       setUploaded(true);
     } catch (error) {
-      // アップロードプロセス中のエラーを処理
       console.error("ファイルのアップロードエラー:", error);
       setLoading(false);
       setUploaded(false);
     }
   };
 
-  // 現在のステートに基づいてJSXをレンダリング
+  // チャット送信先を設定
+  const handleSendChat = () => {
+    let chatRoom = "defaultChatRoom"; // デフォルトのチャットルーム
+
+    // URLに応じてチャットルームを設定
+    if (location.pathname === "/room1.html") {
+      chatRoom = "messages1";
+    } else if (location.pathname === "/room2.html") {
+      chatRoom = "messages2";
+    } else if (location.pathname === "/room3.html") {
+      chatRoom = "messages3";
+    } else if (location.pathname === "/room4.html") {
+      chatRoom = "messages4";
+    }
+
+    // チャットの送信処理（例としてnavigateでURLに遷移する）
+    // 本来はチャット送信のための関数やコンポーネントを呼び出すべきです
+    navigate(`/chat/${chatRoom}`);
+  };
+
+  useEffect(() => {
+    // URLに応じて特定の挙動を実行
+    switch (location.pathname) {
+      // ... 他のケースに対する処理も追加
+      case "/room1.html":
+        // /room1.html の場合の特別な処理
+        console.log("Entered room 1");
+        break;
+      // ...他のケースに対する処理も追加
+      default:
+        // 上記以外の場合の処理
+        console.log("Entered a different room");
+        break;
+    }
+  }, [location.pathname]);
+
   return (
-    <>
-      {loading ? (
-        <h2>アップロード中・・・</h2>
-      ) : (
         <>
-          {isUploaded ? (
-            <>
-              <h2>アップロード完了</h2>
-            </>
+          {loading ? (
+            <h2>アップロード中・・・</h2>
           ) : (
-            <div className="outerBox">
-              <div className="title">
-                <h2>画像アップローダー</h2>
-                <p>JpegかPngの画像ファイル</p>
-              </div>
-              <div className="imageUplodeBox">
-                <div className="imageLogoAndText">
-                  <img src={ImageLogo} alt="imagelogo" />
-                  <p>ここにドラッグ＆ドロップしてね</p>
+            <>
+              {isUploaded ? (
+                <>
+                  <h2>アップロード完了</h2>
+                </>
+              ) : (
+                <div className="outerBox">
+                  <div className="title">
+                    <h2>画像アップローダー</h2>
+                    <p>JpegかPngの画像ファイル</p>
+                  </div>
+                  <div className="imageUplodeBox">
+                    <div className="imageLogoAndText">
+                      <img src={ImageLogo} alt="imagelogo" />
+                      <p>ここにドラッグ＆ドロップしてね</p>
+                    </div>
+                    {/* ファイル選択のためのInputフィールドとイベントハンドラ */}
+                    <input
+                      className="imageUploadInput"
+                      multiple
+                      name="imageURL"
+                      type="file"
+                      accept=".png, .jpeg, .jpg"
+                      onChange={OnFileUploadToFirebase}
+                    />
+                  </div>
+                  <p>または</p>
+                  <div className="buttonContainer">
+                    {/* 関連付けられたInputフィールドを使用したファイル選択のためのボタン */}
+                    <Button variant="contained">
+                      ファイルを選択
+                      <input
+                        className="imageUploadInput"
+                        type="file"
+                        accept=".png, .jpeg, .jpg"
+                        onChange={OnFileUploadToFirebase}
+                      />
+                    </Button>
+                    {/* チャットに戻るための追加のボタン（コメントアウト済み） */}
+                    {/* <Button variant="outlined" onClick={handleReturnToChat}>
+                      チャットに戻る
+                    </Button> */}
+                  </div>
                 </div>
-                {/* ファイル選択のためのInputフィールドとイベントハンドラ */}
-                <input
-                  className="imageUploadInput"
-                  multiple
-                  name="imageURL"
-                  type="file"
-                  accept=".png, .jpeg, .jpg"
-                  onChange={OnFileUploadToFirebase}
-                />
-              </div>
-              <p>または</p>
-              <div className="buttonContainer">
-                {/* 関連付けられたInputフィールドを使用したファイル選択のためのボタン */}
-                <Button variant="contained">
-                  ファイルを選択
-                  <input
-                    className="imageUploadInput"
-                    type="file"
-                    accept=".png, .jpeg, .jpg"
-                    onChange={OnFileUploadToFirebase}
-                  />
-                </Button>
-                {/* チャットに戻るための追加のボタン（コメントアウト済み） */}
-                {/* <Button variant="outlined" onClick={handleReturnToChat}>
-                  チャットに戻る
-                </Button> */}
-              </div>
-            </div>
+              )}
+            </>
           )}
         </>
-      )}
-    </>
-  );
+      );  
 };
-
-// ImageUploaderコンポーネントをデフォルトエクスポート
-export default ImageUploader;
 
 
 
